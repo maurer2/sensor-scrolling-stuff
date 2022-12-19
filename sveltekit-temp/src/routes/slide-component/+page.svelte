bf79ef2c52761363591f2633caeaf8a2418755f4
Martin <mr.mmaurer+work@gmail.com>
2022-12-19 01:14:10 +0000

    Migrated slide component from Vue2 to Svelte

--- sveltekit-temp/src/routes/slide-component/+page.svelte
+++ sveltekit-temp/src/routes/slide-component/+page.svelte
@@ -2,10 +2,8 @@
   import { faker } from '@faker-js/faker';
   import { onMount } from 'svelte';
 
-
-  let pointerIsDown = false;
+  let pointerIsDown;
   let translateX = 0;
-  let transformValue = '';
   let container;
   let image;
 
@@ -18,7 +16,7 @@
   }
 
   function pointerMove(event): void {
-    if (pointerIsDown) {
+    if (!pointerIsDown) {
       return
     }
 
@@ -40,23 +38,21 @@
 
 </script>
 
-<section
-  on:pointerdown={activatePointer}
-  on:pointerup={deactivatePointer}
-  on:pointermove={pointerMove}
-  on:pointerleave={deactivatePointer}
-
->
+<section>
   <h2 class="text-xl pb-4">Slide component</h2>
-
   <p class="pb-4">{faker.lorem.lines()}</p>
-
-  <figure bind:this={container} style:--translateX="{translateX}">
-    <figcaption class="absolute bottom-0 bg-white">
-      Meow! {translateX}%
-    </figcaption>
-    <img src={faker.image.cats(640, 480, false)} alt="Cat" bind:this={image}>
-  </figure>
+  <div class="m-0 p-0 overflow-hidden"
+    on:pointerdown={activatePointer}
+    on:pointerup={deactivatePointer}
+    on:pointermove={pointerMove}
+    on:pointerleave={deactivatePointer}
+  >
+    <figure bind:this={container} class="relative select-none touch-none" style="translate: {translateX}% 0;">
+      <img src={faker.image.cats(960, 640, false)} bind:this={image} class="w-[150%] ml-[-25%] mr-[-25%] max-w-none pointer-events-none" alt="Cat">
+      <figcaption class="absolute inset-x-4 bottom-4 text-center bg-white">
+        Meow!
+      </figcaption>
+    </figure>
+  </div>
 
 </section>
-
